37eecb958faea1dd2a5fecf824f812246dc6fa6e
Anmol Purty <62499408+an3ol@users.noreply.github.com>
2021-07-14 03:03:36 +0530

    Update config.tcl

--- task1/config.tcl
+++ task1/config.tcl
@@ -20,7 +20,7 @@ set ::env(QUIT_ON_TR_DRC) 0
 
 
 set ::env(SYNTH_MAX_FANOUT) 6
-set ::env(FP_CORE_UTIL) 35
+set ::env(FP_CORE_UTIL) 53
 
 set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
 
