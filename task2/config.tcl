acd9d1a38a86e7652f18ab5a01ccaed84ce7790a
Anmol Purty <62499408+an3ol@users.noreply.github.com>
2021-07-14 03:02:36 +0530

    Update config.tcl

--- task2/config.tcl
+++ task2/config.tcl
@@ -5,15 +5,16 @@ set ::env(VERILOG_FILES) "./designs/task2/src/*.v"
 set ::env(CLOCK_PERIOD) "17.000"
 set ::env(CLOCK_PORT) "clk"
 
-set ::env(ROUTING_OPT_ITERS) 5
+set ::env(ROUTING_OPT_ITERS) 4
 
 set ::env(QUIT_ON_ILLEGAL_OVERLAPS) 0
 set ::env(QUIT_ON_LVS_ERROR) 0
 set ::env(QUIT_ON_MAGIC_DRC) 0
 set ::env(QUIT_ON_TR_DRC) 0
+set ::env(DIODE_INSERTION_STRATEGY) 5
 
 set ::env(CLOCK_NET) $::env(CLOCK_PORT)
-set ::env(FP_CORE_UTIL) 35
+set ::env(FP_CORE_UTIL) 45
 set ::env(SYNTH_MAX_FANOUT) 6
 set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
 
